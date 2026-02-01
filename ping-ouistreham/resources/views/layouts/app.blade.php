657af9cb2569b448b53a998417dac3636c37de06
Martin <martin.lemesle@etu.unicaen.fr>
2026-02-02 00:48:03 +0100

    deploy + correction title + favicon

--- ping-ouistreham/resources/views/layouts/app.blade.php
+++ ping-ouistreham/resources/views/layouts/app.blade.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ config('app.name', 'Ping Ouistreham') }}</title>
+    <title>Tournoi de Ouistreham</title>
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 
+    {{-- Fonts --}}
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,700,900" rel="stylesheet" />
 
